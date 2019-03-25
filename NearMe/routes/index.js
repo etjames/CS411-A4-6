81364ae342fd86198c2f45a0fd735b31913bf18c
Andy - Mac <aquan@bu.edu>
2019-03-24 22:14:53 -0400

    homepage.html is opened by default

--- NearMe/routes/index.js
+++ NearMe/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
+let path = require('path');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.sendFile('homepage.html');
+  const homepagePath = (path.join(__dirname , '../views' ,'homepage.html'));
+  res.sendFile(homepagePath);
 });
 
 module.exports = router;
