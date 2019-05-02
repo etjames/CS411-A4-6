f1591f7602f20e088886ed9222c505e9b4c5a594
Andrew I Quan <aquan@bu.edu>
2019-05-01 20:39:21 -0400

    fixed merge conflict

--- NearMe/oauth/auth.js
+++ NearMe/oauth/auth.js
@@ -17,12 +17,6 @@ module.exports = function (passport) {
             clientID: config.google.clientID,
             clientSecret: config.google.clientSecret
     }, (token, refreshToken, profile, done) => {
-<<<<<<< HEAD:NearMe/views/auth.js
-=======
-        //console.log(profile.emails[0].value);
-        //console.log(token);
-        //console.log(profile.id);
->>>>>>> 60ba87cd321425f1ccbae3137b01ffd3cff145c9:NearMe/oauth/auth.js
         User.findOne({'id': profile.id}, 
         function(err, user) {
            if (!user) {
@@ -46,9 +40,5 @@ module.exports = function (passport) {
 }
         )
    );
-<<<<<<< HEAD:NearMe/views/auth.js
-    }
 
-=======
-}
->>>>>>> 60ba87cd321425f1ccbae3137b01ffd3cff145c9:NearMe/oauth/auth.js
+    }
